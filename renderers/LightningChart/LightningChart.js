d9d13187926c33258f662e6651ea4da36ecca1fb
Stereokai <tom@stereokai.com>
2022-05-09 22:23:39 +0200

    Support no. of samples + time range updates

--- renderers/LightningChart/LightningChart.js
+++ renderers/LightningChart/LightningChart.js
@@ -9,7 +9,7 @@ import {
 
 let chart, series, xAxis;
 const DEFAULT_X_RANGE_MS = 30 * 1000;
-let signals;
+let graphs;
 let isLoaded = false;
 export function on() {}
 
@@ -19,99 +19,54 @@ export function init() {
       container: document.querySelector("#chart"),
       numberOfColumns: 1,
       numberOfRows: channels.length,
-      theme: Themes.darkGold,
+      theme: Themes.light,
     })
     .setSplitterStyle(emptyLine);
 
-  signals = channels.map((signal, iSignal) => {
-    const chart = dashboard
-      .createChartXY({
-        columnIndex: 0,
-        rowIndex: iSignal,
-
-        columnSpan: 1,
-        rowSpan: 1,
-        disableAnimations: true,
-      })
-      .setTitle("")
-      .setPadding({ top: 0, bottom: 0 })
-      // .setAutoCursorMode(AutoCursorModes.disabled)
-      .setBackgroundStrokeStyle(emptyLine);
-    // .setMouseInteractions(false);
-
-    const axisX = chart
-      .getDefaultAxisX()
-      .setTickStrategy(AxisTickStrategies.Empty)
-      .setStrokeStyle(emptyLine);
-    // .setScrollStrategy(AxisScrollStrategies.progressive)
-    // .setInterval();
-    const axisY = chart
-      .getDefaultAxisY()
-      .setMouseInteractions(false)
-      .setChartInteractions(false)
-      .setChartInteractionFitByDrag(false)
-      .setChartInteractionZoomByDrag(false)
-      .setChartInteractionPanByDrag(false)
-      .setChartInteractionZoomByWheel(false)
-      .setTickStrategy(AxisTickStrategies.Empty)
-      .setStrokeStyle(emptyLine)
-      .setTitle(signal.name)
-      .setTitleRotation(0)
-      .setThickness(60)
-      .setAnimationZoom(undefined);
-    // .fit()
-    // .onScaleChange((...args) => {
-    //   console.log(...args);
-    // });
-
-    const series = chart.addLineSeries({
-      dataPattern: { pattern: "ProgressiveX" },
-      automaticColorIndex: iSignal,
-    });
-    // .setName(`Channel ${iSignal + 1}`)
-    // .setDataCleaning({ minDataPointCount: 10000 });
-
-    return { chart, series, axisX, axisY };
+  graphs = channels.map((channel, i) => {
+    return addChannel(dashboard, channel, i);
   });
-  // const channelTop = signals[0];
 
-  // channelTop.chart;
-  // .setTitle("Multi-channel real-time monitoring (10 chs, 1000 Hz)")
-  // .setPadding({ top: 8 });
+  synchronizeAxisIntervals(...graphs.map((ch) => ch.xAxis));
 
-  synchronizeAxisIntervals(...signals.map((ch) => ch.axisX));
-  // axisX.setInterval(0, 100);
+  window.graphs = graphs;
+  window.dashboard = dashboard;
 }
 
 export function update(dataset, timeSeries) {
-  if (isLoaded) return;
-  isLoaded = true;
-  signals.forEach((signal, i) => {
+  graphs.forEach((signal, i) => {
     // if (i < 5) return;
     // console.log("adding", dataset[i]);
     console.log(dataset[i].length, timeSeries.length);
+    signal.series.clear();
     signal.series.addArraysXY(timeSeries, dataset[i]);
     // signal.series.addArrayY(dataset[i]);
   });
 
-  const channelBottom = signals[signals.length - 1];
+  const channelBottom = graphs[graphs.length - 1];
   const baseDate = new Date();
   baseDate.setDate(baseDate.getDate() - 1);
   baseDate.setHours(22);
-  const axisX = channelBottom.axisX.setTickStrategy(
+  const xAxis = channelBottom.xAxis.setTickStrategy(
     AxisTickStrategies.DateTime,
     (ticks) => ticks.setDateOrigin(baseDate)
     // .setMajorTickStyle((major) => major.setGridStrokeStyle(emptyLine))
     // .setMinorTickStyle((minor) => minor.setGridStrokeStyle(emptyLine))
   );
-  axisX.onScaleChange(() => {
-    signals.forEach((signal, i) => {
-      signal.axisY.setInterval(channels[i].min, channels[i].max);
+  xAxis.setInterval(
+    timeSeries[0],
+    timeSeries[timeSeries.length - 1],
+    false,
+    true
+  );
+  xAxis.onScaleChange(() => {
+    graphs.forEach((signal, i) => {
+      signal.yAxis.setInterval(channels[i].min, channels[i].max);
     });
   });
-  // axisX.setInterval(
-  //     signals[9].series.getXMin(),
-  //     signals[9].series.getXMax(),
+  // xAxis.setInterval(
+  //     graphs[9].series.getXMin(),
+  //     graphs[9].series.getXMax(),
   //     false,
   //     true
   //   );
@@ -121,3 +76,48 @@ export function buildModel(channelsDataArray, timeSeries) {}
 
 export function showLoading() {}
 export function hideLoading() {}
+
+function addChannel(dashboard, channel, channelIndex) {
+  const chart = dashboard
+    .createChartXY({
+      columnIndex: 0,
+      rowIndex: channelIndex,
+      columnSpan: 1,
+      rowSpan: 1,
+      disableAnimations: true,
+    })
+    .setTitle("")
+    .setPadding({ top: 0, bottom: 0 })
+    // .setAutoCursorMode(AutoCursorModes.disabled)
+    .setBackgroundStrokeStyle(emptyLine);
+  // .setMouseInteractions(false);
+
+  const xAxis = chart
+    .getDefaultAxisX()
+    .setTickStrategy(AxisTickStrategies.Empty)
+    .setStrokeStyle(emptyLine);
+
+  const yAxis = chart
+    .getDefaultAxisY()
+    .setMouseInteractions(false)
+    .setChartInteractions(false)
+    .setTickStrategy(AxisTickStrategies.Empty)
+    .setStrokeStyle(emptyLine)
+    .setTitle(channel.name)
+    .setTitleRotation(0)
+    .setThickness(60)
+    .setAnimationZoom(undefined);
+  // .fit()
+  // .onScaleChange((...args) => {
+  //   console.log(...args);
+  // });
+
+  const series = chart.addLineSeries({
+    dataPattern: { pattern: "ProgressiveX" },
+    automaticColorIndex: channelIndex,
+  });
+  // .setName(`Channel ${iSignal + 1}`)
+  // .setDataCleaning({ minDataPointCount: 10000 });
+
+  return { chart, series, xAxis, yAxis };
+}
