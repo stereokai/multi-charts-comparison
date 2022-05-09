4a91525dd02759115d78653efc6318481165e472
Stereokai <tom@stereokai.com>
2022-05-09 13:53:20 +0200

    Initial LightningChart scaffolding

---
+++ renderers/LightningChart/LightningChart.js
@@ -0,0 +1,13 @@
+import lightningChart from "@arction/lcjs";
+console.log("hi from lc", lightningChart);
+
+export function on() {}
+
+export function init() {}
+
+export function update(dataset, timeSeries) {}
+
+export function buildModel(channelsDataArray, timeSeries) {}
+
+export function showLoading() {}
+export function hideLoading() {}
