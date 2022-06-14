5deb5dc6ee4af5a61d4a1d107a28d2a8bc07e6d7
Stereokai <tom@stereokai.com>
2022-06-14 19:14:32 +0200

    Plug initial data to Highcharts

--- renderers/Highcharts/Highcharts.js
+++ renderers/Highcharts/Highcharts.js
@@ -6,105 +6,6 @@ import Highcharts from "highcharts/highstock";
 import Data from "highcharts/modules/data";
 Data(Highcharts);
 
-Highcharts.getJSON(
-  "https://demo-live-data.highcharts.com/aapl-ohlcv.json",
-  function (data) {
-    // split the data set into ohlc and volume
-    var ohlc = [],
-      volume = [],
-      dataLength = data.length,
-      // set the allowed units for data grouping
-      groupingUnits = [
-        [
-          "week", // unit name
-          [1], // allowed multiples
-        ],
-        ["month", [1, 2, 3, 4, 6]],
-      ],
-      i = 0;
-
-    for (i; i < dataLength; i += 1) {
-      ohlc.push([
-        data[i][0], // the date
-        data[i][1], // open
-        data[i][2], // high
-        data[i][3], // low
-        data[i][4], // close
-      ]);
-
-      volume.push([
-        data[i][0], // the date
-        data[i][5], // the volume
-      ]);
-    }
-
-    // create the chart
-    Highcharts.stockChart("chart", {
-      rangeSelector: {
-        selected: 1,
-      },
-
-      title: {
-        text: "AAPL Historical",
-      },
-
-      yAxis: [
-        {
-          labels: {
-            align: "right",
-            x: -3,
-          },
-          title: {
-            text: "OHLC",
-          },
-          height: "60%",
-          lineWidth: 2,
-          resize: {
-            enabled: true,
-          },
-        },
-        {
-          labels: {
-            align: "right",
-            x: -3,
-          },
-          title: {
-            text: "Volume",
-          },
-          top: "65%",
-          height: "35%",
-          offset: 0,
-          lineWidth: 2,
-        },
-      ],
-
-      tooltip: {
-        split: true,
-      },
-
-      series: [
-        {
-          type: "candlestick",
-          name: "AAPL",
-          data: ohlc,
-          dataGrouping: {
-            units: groupingUnits,
-          },
-        },
-        {
-          type: "column",
-          name: "Volume",
-          data: volume,
-          yAxis: 1,
-          dataGrouping: {
-            units: groupingUnits,
-          },
-        },
-      ],
-    });
-  }
-);
-
 export const graphEvents = graphEventsFactory();
 export function on(...args) {
   graphEvents.on(...args);
@@ -112,7 +13,45 @@ export function on(...args) {
 
 export function init(container) {}
 
-export function update(dataset, timeSeries) {}
+export function update(dataset, timeSeries) {
+  const GAP = 5;
+  const yAxis = [...Array(dataset.length)].map((_, i, all) => {
+    const height = 100 / all.length - GAP / all.length;
+    const top = i * (height + GAP / all.length);
+    return {
+      top: `${top}%`,
+      height: `${height}%`,
+    };
+  });
+
+  for (let i = 0; i < timeSeries.length; i++) {
+    for (let j = 0; j < dataset.length; j++) {
+      dataset[j].data[i] = [timeSeries[i], dataset[j].data[i]];
+    }
+  }
+
+  const series = [...Array(dataset.length)].map((_, i, all) => {
+    return {
+      lineWidth: 1,
+      name: `${i}`,
+      data: dataset[i].data,
+      yAxis: i,
+    };
+  });
+
+  console.log("update", dataset, timeSeries, yAxis, series);
+
+  Highcharts.stockChart("chart", {
+    rangeSelector: {
+      selected: 1,
+    },
+    yAxis,
+    series,
+    tooltip: {
+      split: true,
+    },
+  });
+}
 
 export function buildModel(incomingDataset, timeSeries) {}
 
