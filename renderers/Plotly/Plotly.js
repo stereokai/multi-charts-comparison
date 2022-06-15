11b55f664cc4b59f04f99d83f85955f1bb6e0291
Stereokai <tom@stereokai.com>
2022-06-15 14:04:50 +0200

    Plug plotly to events

--- renderers/Plotly/Plotly.js
+++ renderers/Plotly/Plotly.js
@@ -1,15 +1,29 @@
-import { graphEventsFactory } from "@/Graphs/graphEvents.js";
+import GRAPH_EVENTS, { graphEventsFactory } from "@/Graphs/graphEvents.js";
+import * as Plotly from "plotly.js-dist";
 
 export const graphEvents = graphEventsFactory();
 export function on(...args) {
   graphEvents.on(...args);
 }
 
-let isInitialized = false;
+let lastEvent = null;
+let hasInitialized = false;
+let chart;
 
-export function init(container) {}
+export function init(container) {
+  chart = container;
+  lastEvent = {
+    timestamp: performance.now(),
+  };
+}
 
 export function update(dataset, timeSeries) {
+  if (hasInitialized) {
+    lastEvent = {
+      timestamp: performance.now(),
+    };
+  }
+
   timeSeries = timeSeries.map((timestamp) => new Date(timestamp));
 
   var data = dataset.map((channel, i, dataset) => ({
@@ -31,9 +45,9 @@ export function update(dataset, timeSeries) {
 
   var layout = {
     margin: {
-      t: 0,
+      t: 50,
       b: 0,
-      l: 0,
+      l: 50,
       r: 0,
     },
     grid: {
@@ -50,41 +64,36 @@ export function update(dataset, timeSeries) {
     annotations: false,
   };
 
-  Plotly[!isInitialized ? "newPlot" : "react"]("chart", data, layout, {
-    scrollZoom: true,
-    responsive: true,
-  });
-  isInitialized = true;
+  if (!hasInitialized) {
+    Plotly.newPlot(chart, data, layout, {
+      scrollZoom: true,
+      responsive: true,
+    });
+    chart.on("plotly_afterplot", reportRenderEvent);
+  } else {
+    Plotly.react(chart, data, layout, {
+      scrollZoom: true,
+      responsive: true,
+    });
+  }
 }
-
-export function buildModel(incomingDataset, timeSeries) {}
 
 export function showLoading() {}
 export function hideLoading() {}
 
-import * as Plotly from "plotly.js-dist";
-
-var trace1 = {
-  x: [0, 1, 2],
-  y: [10, 11, 12],
-  xaxis: "x",
-  type: "scatter",
-};
-
-var trace2 = {
-  x: [0, 1, 2],
-  y: [100, 110, 120],
-  xaxis: "x",
-  yaxis: "y2",
-  type: "scatter",
-};
-
-var trace3 = {
-  x: [0, 1, 2],
-  y: [1000, 1100, 1200],
-  xaxis: "x",
-  yaxis: "y3",
-  type: "scatter",
-};
-
-var data = [trace1, trace2, trace3];
+function reportRenderEvent() {
+  const duration = (performance.now() - lastEvent.timestamp) / 1000;
+  if (!hasInitialized) {
+    hasInitialized = true;
+    graphEvents.init({
+      duration,
+      type: GRAPH_EVENTS.init,
+    });
+  } else {
+    graphEvents.render({
+      duration,
+      type: GRAPH_EVENTS.render,
+    });
+  }
+  lastEvent = null;
+}
